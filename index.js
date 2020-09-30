7812d11eaa3a1467e9379b393a92d1d0c0580870
MOBOLA MORONTO <t2wycemo@gmail.com>
2020-09-30 04:01:26 +0000

    Done.

--- index.js
+++ index.js
@@ -10,21 +10,82 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var item = createCartItem(item)
+  getCart().push(item)
+  return `${item.itemName} has been added to your cart.`
 }
 
 function viewCart() {
-  // write your code here
+  return getCart().length === 0 ? "Your shopping cart is empty." : createCartDescription()
 }
 
 function total() {
-  // write your code here
+  var sum = sumUpPrices()
+  return sum
 }
 
-function removeFromCart(item) {
-  // write your code here
+function removeFromCart(itemName) {
+  var itemToRemove = searchCartForItemToRemove(itemName)
+  return itemToRemove ? removeItemFromCart(itemToRemove) : notifyUserThereIsNoItemToRemove()
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (arguments[0] == undefined) {
+    return "Sorry, we don't have a credit card on file for you."
+  } else {
+    var sumToCharge = total()
+    setCart([])
+    return `Your total cost is $${sumToCharge}, which will be charged to the card ${cardNumber}.`
+  }
 }
+
+// Helper Functions
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function createCartItem(itemName) {
+  return { itemName: itemName, itemPrice: getRandomInt(1, 100) }
+}
+
+function createCartDescription() {
+  var cartDescription = 'In your cart, you have '
+  if (getCart().length >= 1 ) {
+    cartDescription += `${getCart()[0].itemName} at $${getCart()[0].itemPrice}`
+  }
+  if (getCart().length >= 2 ) {
+    var middleCartItemsDescription = ''
+    for (var i = 1; i < getCart().length -1; i++ ) {
+    middleCartItemsDescription += `, ${getCart()[i].itemName} at $${getCart()[i].itemPrice}`
+    }
+    cartDescription += `${middleCartItemsDescription}, and ${getCart()[getCart().length - 1].itemName} at $${getCart()[getCart().length-1].itemPrice}`
+  }
+  return `${cartDescription}.`
+}
+
+function sumUpPrices() {
+  var sum = 0
+  for ( var i = 0; i < getCart().length; i++) {
+    sum = sum + getCart()[i].itemPrice
+  }
+  return sum
+}
+
+function searchCartForItemToRemove(itemName) {
+  var searchResult
+  for (var i=0; i < getCart().length; i++) {
+    if (getCart()[i].itemName === itemName) {searchResult = getCart()[i]}
+  }
+  return searchResult
+}
+
+function removeItemFromCart(itemToRemove) {
+  var indexOfItemToRemove = cart.indexOf(itemToRemove)
+  getCart().splice(indexOfItemToRemove,1)
+}
+
+function notifyUserThereIsNoItemToRemove() {
+  return 'That item is not in your cart.'
+}
+
